aabf7ed55d09bf36b1f4823469f98050c8843d84
Jeanne Choo <jchoo@aisingapore.org>
2019-02-28 16:46:43 +0800

    fixed syntax error

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -15,4 +15,4 @@ class TestCalculator(unittest.TestCase):
 
 class TestCalculator(unittest.TestCase):
     def test_5_and_1_should_return_5(self):
-        self.assertEqual(5, divide(5, 1)
+        self.assertEqual(5, divide(5, 1))
