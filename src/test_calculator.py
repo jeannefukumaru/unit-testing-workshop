dde19e242aa7998b177acf86096e06abf7524f36
Jeanne Choo <jchoo@aisingapore.org>
2019-02-28 16:47:51 +0800

    renamed divide test

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -14,5 +14,5 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(4, mult(2, 2))
 
 class TestCalculator(unittest.TestCase):
-    def test_5_and_1_should_return_5(self):
+    def test_divide_5_and_1_should_return_5(self):
         self.assertEqual(5, divide(5, 1))
