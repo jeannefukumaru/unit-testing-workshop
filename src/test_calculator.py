f021bb74c9800636e39a8a8faae61ec73b557433
Jeanne Choo <jchoo@aisingapore.org>
2019-02-28 16:42:07 +0800

    added tests to test_calculator

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -3,4 +3,16 @@ from calculator import add
 
 class TestCalculator(unittest.TestCase):
     def test_add_1_and_1_should_return_2(self):
+        self.assertEqual(2, add(1, 1))
+
+class TestCalculator(unittest.TestCase):
+    def test_subtract_2_and_1_should_return_1(self):
+        self.assertEqual(1, subtract(2,1))
+
+class TestCalculator(unittest.TestCase):
+    def test_multiple_2_and_2_should_return_4(self):
+        self.assertEqual(4, mult(2, 2))
+
+class TestCalculator(unittest.TestCase):
+    def test_5_and_1_should_return_1(self):
         self.assertEqual(add(1, 1), 2)
